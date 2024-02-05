d9bb23230e41c46f89cf9a197d6a988e40202e20
Quincy Jones <rqjones@asu.edu>
2024-02-05 14:07:48 -0700

    removed branches and added public view function's

--- contracts/repository.sol
+++ contracts/repository.sol
@@ -10,16 +10,20 @@ import "@openzeppelin/contracts/token/IERC20/IERC20.sol";
 /// @dev a free open source github alternative
 
 contract GitXDC is ERC1155 {
-    uint versionCount = 0;
-    uint branch = 0;
-    string description;
+    uint public versionCount = 0;
+    string public branch = "MAIN";
+    string public description;
+    string public handlerToken = 0;
+    uint public totalRepoKeys;
+    uint public totalBranches = 0;
+    //maybe place award amount havent mad mind up yet
 
-    mapping(uint => Repo) public repo;
+    mapping(string => Repo) public repo;
     mapping(uint => Versions) public version;
-    mapping(string => Branches) public branch;
-    mapping(uint => Branches) public branch;
+    mapping(uint => AllPullRequest) public pull;
 
     event edit(uint _timestamp,_msg, uint _version);
+    event comment(_title,_comment);
 
     struct Repo {
         string name;
@@ -32,19 +36,21 @@ contract GitXDC is ERC1155 {
         uint versionNumber;
     }
 
-    struct Branches{
-        string branch;
-    }
-
     struct AllPullRequest{
         address repo;
         string title;
         string messeage;
     }
 
+    struct Branches{
+        address contracts;
+        string branchName;
+        string description;
+    }
 
+    //check to see it user has repo key
     modifier Handler{
-
+        require(balanceOf(msg.sender,handlerToken) >= 1, "currently do not hold repo key");
         _;
     }
 
@@ -54,34 +60,59 @@ contract GitXDC is ERC1155 {
     /// @return Documents the return variables of a contract’s function state variable
     /// @inheritdoc	Copies all missing tags from the base function (must be followed by the contract name)
 
-    constructor(string memory _repoName,string memory _description, string[] memory _code, string[] memory _filenames,_URI) ERC1155(_URI) {
+    constructor(string memory _repoName,uint _totalRepoKeys,string memory _description, string[] memory _code, string[] memory _filenames,_URI) ERC1155(_URI) {
+        _mint(msg.sender, handlerToken,_totalRepoKeys, "");
+
+        totalRepoKeys = _totalRepoKeys;
         name = _repoName;
         description = _description;
+
         version[versionCount] = Versions(_code, _filenames, versionCount);
         repo[branch] = Repo(_repoName, version[versionCount]);
+
         versionCount++;
     }
 
-    function editRepo(uint _branch,string _code,string _filename) public Handler returns(bool,uint){
-        for(uint i = 0;i < repo[_branch].versions[_version].filenames.length;i++){
-            if(repo[_branch].versions[_version].filenames[i] == _filename){
-                repo[_branch].versions[_version].code[i] = _code;
-                repo[_branch].versions[_version].filenames[i] = _filename;
+    function editRepo(string _code,string _filename) public Handler returns(bool){
+        bool isNewFile = true;
+        //checks to see if file is present and edits the code
+        for(uint i = 0;i < repo[branch].versions[_version].filenames.length;i++){
+            if(repo[branch].versions[_version].filenames[i] == _filename){
+                repo[branch].versions[_version].code[i] = _code;
+                isNewFile = false;
+            } 
+            // if file is not found create file and place code as next item in array
+            //runs if code if file is not found
+            if(i == repo[branch].versions[_version].filenames.length && isNewFile == true){
+                repo[branch].versions[_version].code[i+1] = _code;
+                repo[branch].versions[_version].filenames[i+1] = _filename;
             }
         }
         versionCount++;
-
         emit edit(block.timestamp,versionCount);
-        return (true,versionCount);
+        return (true);
     }
 
-    function ApproovePullRequest(address _repo)public  Handler returns(bool){}
-
-
+    function MergePullRequest(address _repo,string _title,string _comments, bool _merge)public  Handler returns(bool){
+        emit comment(_title,_comment);
+        emit edit(block.timestamp,versionCount);
+        return true;
+    }
+    //create a pull request
     function OpenPullRequest(address _repo,string memory _title, string memory _description)public returns(bool){}
+    //view all pull request
     function ViewAllPullRequest()public view returns(bool){}
+    //view all generated contracts that have forked a current version of the code
+    function ViewAllForks(string memory _forkName )public view returns(bool){
+        require(_forkName != "MAIN","you cant create a fork with the same branch name as MAIN");
+        require(_forkName != branch,"you cant create a fork with the same branch name as the current fork");
+    }
+    //generates a new contract with version of code inside
+    function CreateFork()public view returns(bool){}
 
-    function CloneRepo(uint _branch, uint _version) public view returns (string[] memory, string[] memory) {
-        return (repo[_branch].versions[_version].code, repo[_branch].versions[_version].filenames);
+    function forwardBranch(uint _intBranch)public view returns(bool,uint,string memory){}
+
+    function CloneRepo(uint _version) public view returns (string[] memory, string[] memory) {
+        return (repo[branch].versions[_version].code, repo[branch].versions[_version].filenames);
     }
 }
