df8f676528e988b97a5ad80eb4c9584fad0e7d71
Quincy Jones <rqjones@asu.edu>
2024-03-19 22:20:04 -0700

    Update Ledger.sol

--- contracts/Ledger.sol
+++ contracts/Ledger.sol
@@ -7,10 +7,12 @@ import "./KeyManager.sol";
 /// @notice this is a contract that keeps track of each move in a game
 
 abstract contract Ledger{
-    uint totalRounds = 0;
+    uint totalMoves = 0;
+    Moves[] moves;
+    address[] teams;
+    uint public maxTeams = 2;
 
     mapping(uint => Moves) public move;
-    mapping(uint => MoveLedger) private eachRound;
     struct Moves{
         address team;
         string move;
@@ -18,23 +20,40 @@ abstract contract Ledger{
         bool exist;
     }
 
-    struct MoveLedger{
-        Moves[] moves;
-        address[] teams;
+    constructor(address[] memory _totalTeams) {
+        require(_totalTeams.length > 0, "No teams have been added");
+        require(_totalTeams.length == maxTeams, "Only two teams are allowed");
+        teams = _totalTeams;
     }
+
+    modifier toggleTeamTurn(address _teamName){
+        require(_teamName == teams[0] || _teamName == teams[1], "Team not authorized");
+        require(teams.length > 0, "No teams have been added");
+        require(maxTeams < teams.length, "All teams have made a move");
+        require(keccak256(abi.encodePacked(move[totalMoves - 1].team)) == keccak256(abi.encodePacked(_teamName)), "It is not your turn");
+        _;
+    }
+
     function getAllMoves() public view returns (Moves[] memory) {
-        Moves[] memory allMoves = new Moves[](totalRounds);
-        for (uint i = 0; i < totalRounds; i++) {
-            allMoves[i] = eachRound[i].moves;
+        Moves[] memory allMoves = new Moves[](totalMoves);
+        for (uint i = 0; i < totalMoves; i++) {
+            allMoves[i] = move[i];
         }
         return allMoves;
     }
 
-    function getAllTeams() public view returns (address[] memory) {
-        address[] memory allTeams = new address[](totalRounds);
-        for (uint i = 0; i < totalRounds; i++) {
-            allTeams[i] = eachRound[i].teams;
-        }
-        return allTeams;
+    function makeMove(string memory _move) public virtual toggleTeamTurn(msg.sender) returns(bool){
+        require(bytes(_move).length > 0, "Move cannot be empty");
+
+        Moves memory newMove = Moves({
+            team: msg.sender,
+            move: _move,
+            time: block.timestamp,
+            exist: true
+        });
+
+        move[totalMoves] = newMove;
+        totalMoves++;
+        return true;
     }
 }
