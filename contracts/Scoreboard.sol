b35c7c3f26bb9169c9a2b28b43b01f59d08a3130
Quincy Jones <rqjones@asu.edu>
2024-03-21 09:16:50 -0700

    make functions internal

--- contracts/Scoreboard.sol
+++ contracts/Scoreboard.sol
@@ -7,9 +7,10 @@ import "./KeyManager.sol";
 /// @notice this is a contract that keeps track of score
 /// @dev a free open source game counter
 
-contract Scoreboard is Client{
-    string public appName;
+contract Scoreboard{
     uint public gameCounter = 0;
+    string public scoreboard = "Default";
+
     mapping(string => Teams) public team;
     mapping(string => Games) public games;
     mapping(uint => Games) public gameList;
@@ -37,16 +38,12 @@ contract Scoreboard is Client{
         bool exist;
     }
 
-    constructor(string memory _appName,uint _totalKeys,string memory _URI) Client(_totalKeys, 0x0000000000000000000000000000000000000000, false, _URI) {
-        appName = _appName;
-    }
-
     modifier gameStarted(string memory _gameName){
         require(games[_gameName].isGameStarted == true, "Game has not started");
         _;
     }
 
-    function createGame(string memory _gameName,uint _winCondition) public onlyClient() returns(bool){
+    function createGame(string memory _gameName,uint _winCondition) internal virtual returns(bool){
         require(games[_gameName].exist == false, "Game already exists");
         Games memory newGame = Games({
             gameName: _gameName,
@@ -73,7 +70,7 @@ contract Scoreboard is Client{
         return true;
     }
 
-    function createTeam(string memory _gameName,string memory _teamName) public onlyClient() gameStarted(_gameName) returns(bool){
+    function createTeam(string memory _gameName,string memory _teamName) internal virtual gameStarted(_gameName) returns(bool){
         require(team[_teamName].exist == false, "Team already exist");
         team[_teamName] = Teams({
             teamName: _teamName,
@@ -90,13 +87,13 @@ contract Scoreboard is Client{
         return true;
     }
 
-    function startGame(string memory _gameName) public onlyClient() gameStarted(_gameName) returns(bool){
+    function startGame(string memory _gameName) internal virtual gameStarted(_gameName) returns(bool){
         games[_gameName].isGameStarted = true;
         emit gameStatus(_gameName, "New Game","Game has started");
         return true;
     }
 
-    function score(string memory _gameName,string memory _teamName,uint _score,bool _allocation) public onlyClient() gameStarted(_gameName) returns(bool){
+    function score(string memory _gameName,string memory _teamName,uint _score,bool _allocation) internal virtual gameStarted(_gameName) returns(bool){
         require(team[_teamName].exist == true, "Team does not exist");
         if (_allocation == true) {
             team[_teamName].teamData.score += _score;
@@ -108,7 +105,7 @@ contract Scoreboard is Client{
         }
         return true;
     }
-    
+
     function viewAllGames() public view returns(Games[] memory){
         Games[] memory fullGameList = new Games[](gameCounter);
         for(uint i = 0; i < gameCounter; i++){
@@ -126,13 +123,13 @@ contract Scoreboard is Client{
     }
 
     function isOver(string memory _teamName) internal {
-        if(team[_teamName].teamData.score == games[appName].winCondition){
-            games[appName].isOver = true;
-            games[appName].winner = team[_teamName];
+        if(team[_teamName].teamData.score == games[scoreboard].winCondition){
+            games[scoreboard].isOver = true;
+            games[scoreboard].winner = team[_teamName];
         }
     }
 
-    function defaultWinner(string memory _gameName,string memory _teamName) public onlyClient() gameStarted(_gameName) returns(bool){
+    function defaultWinner(string memory _gameName,string memory _teamName) internal virtual gameStarted(_gameName) returns(bool){
         require(games[_gameName].isOver == false, "Game is already over");
         require(games[_gameName].exist == true, "Game does not exist");
         require(team[_teamName].exist == true, "Team does not exist");
